a71ab478905ca34964733b222f1c4e8ec20edab4
Muondo Vladislav <muondove@gmail.com>
2023-02-02 15:00:11 +0200

    home layout wip

--- src/components/scripts/Home.js
+++ src/components/scripts/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "../styles/Home.module.css";
 
 function Home()
 {
     return(
-        <div className="homeContainer">
-            {"Welcome to the jungle, baby."}
+        <div className={styles.homeContainer}>
+            <img alt="main pic" src={require("/Users/s2200741/Documents/Repositories/portfolio_project/src/assets/picture.png")}/>
+            <h2>{"Hello! My name is Muondo Vladislav Emerson"}</h2>
         </div>
     )
 }
