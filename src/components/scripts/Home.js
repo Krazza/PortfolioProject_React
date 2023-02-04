89abe833ea98b7a9d3620c8b94b1c5859dc59c53
Muondo Vladislav <muondove@gmail.com>
2023-02-04 16:49:28 +0200

    home layout wip 2

--- src/components/scripts/Home.js
+++ src/components/scripts/Home.js
@@ -5,7 +5,7 @@ function Home()
 {
     return(
         <div className={styles.homeContainer}>
-            <img alt="main pic" src={require("/Users/s2200741/Documents/Repositories/portfolio_project/src/assets/picture.png")}/>
+            <img alt="main pic" src={require("/Users/s2200741/Documents/Repositories/portfolio_project/src/assets/me.JPG")}/>
             <h2>{"Hello! My name is Muondo Vladislav Emerson"}</h2>
         </div>
     )
